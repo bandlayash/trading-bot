9b98326ddeafb78745b85d181f114d43b5cd2a66
ybandla <bandla.yash@gmail.com>
2025-12-01 12:18:51 -0600

    updated build-layer to install libraries properly and referenced layer in template.yaml

--- build-layer.sh
+++ build-layer.sh
@@ -5,20 +5,20 @@ set -e
 rm -rf python layer
 mkdir -p python
 
-# Build inside Amazon Linux 2023 (matches Lambda)
+echo "Building Lambda layer dependencies in Amazon Linux 2023..."
+
+# Build dependencies inside Amazon Linux 2023
 docker run --rm \
   -v "$PWD":/var/task \
   -w /var/task \
   public.ecr.aws/amazonlinux/amazonlinux:2023 \
   /bin/bash -c "
-    yum update -y &&
-    yum install -y python3 python3-devel gcc gcc-c++ make \
-                    tar gzip unzip findutils
+    yum update -y
+    yum install -y python3 python3-devel gcc gcc-c++ make tar gzip unzip findutils
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt -t python/
+  "
 
-    pip3 install --upgrade pip
-    pip3 install -r requirements.txt -t python/
-"
-
-# Zip layer
+# Zip the layer
 zip -r layer.zip python/
-echo "✓ Layer built: layer.zip"
+echo "✓ Lambda layer built: layer.zip"
